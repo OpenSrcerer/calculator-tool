34d6b836182baab7fdd6b4b484a391b838376bc7
OpenSrcerer <weedpot@fhf.edu.al>
2020-11-14 16:36:47 +0200

    Added convenience and paint functions along with ActionEvent builders.

--- Coffee/src/main/java/cs105Project/userInterface/PanelComponents.java
+++ Coffee/src/main/java/cs105Project/userInterface/PanelComponents.java
@@ -1,15 +1,16 @@
 package cs105Project.userInterface;
 
-import com.sun.tools.javac.Main;
-import cs105Project.actions.Request;
-import cs105Project.actions.RequestManager;
+import cs105Project.actions.exactCubes.ExactCubesRequest;
+import cs105Project.managers.RequestManager;
 import cs105Project.userInterface.panels.ButtonType;
+import cs105Project.userInterface.panels.ExactCubes;
+import cs105Project.userInterface.panels.Selection;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
-public abstract class PanelComponents {
+public final class PanelComponents {
 
     // Stylizing
     public static final Color
@@ -19,17 +20,29 @@ public abstract class PanelComponents {
 
     // Fonts
     public static final Font actionFont = new Font("Arial", Font.BOLD, 15);
+    public static final Font outputFont = new Font("Arial", Font.ITALIC, 13);
 
     public static JButton getButton(String buttonName, ButtonType type) {
         JButton button = new JButton();
+        setButtonPalette(buttonName, button);
+        button.addActionListener(getListener(type));
+        return button;
+    }
+
+    public static JButton getButton(String buttonName, ButtonType type, JTextArea outputField, JTextField inputField) {
+        JButton button = new JButton();
+        setButtonPalette(buttonName, button);
+        button.addActionListener(getListener(type, outputField, inputField));
+        return button;
+    }
+
+    public static void setButtonPalette(String buttonName, JButton button) {
         button.setText("<html>" + buttonName + "</html>");
         button.setOpaque(true);
         button.setBorderPainted(false);
         button.setBackground(discordGray);
         button.setForeground(discordLightGray);
         button.setFont(actionFont);
-        button.addActionListener(getListener(type));
-        return button;
     }
 
     public static JSeparator getJSeparator() {
@@ -45,10 +58,29 @@ public abstract class PanelComponents {
         return panel;
     }
 
+    public static JTextArea getJTextArea(int rows, int cols) {
+        JTextArea area = new JTextArea(rows, cols);
+        area.setFont(outputFont);
+        area.setBackground(discordGrayer);
+        area.setForeground(discordLightGray);
+        return area;
+    }
+
+    public static JTextField getJTextField(String defaultText, int cols) {
+        JTextField field = new JTextField(defaultText, cols);
+        field.setFont(outputFont);
+        field.setBackground(discordGrayer);
+        field.setForeground(discordLightGray);
+        return field;
+    }
+
+    public static void setBackgrounds(JPanel... panels) {
+        for (JPanel panel : panels) {
+            panel.setBackground(discordGray);
+        }
+    }
+
     private static ActionListener getListener(ButtonType type) {
-
-        Request request = null;
-
         /*if (type == ButtonType.HELP) {
 
         } else if (type == ButtonType.CREDITS) {
@@ -56,7 +88,9 @@ public abstract class PanelComponents {
         } else*/ if (type == ButtonType.EXACTCUBES) {
             return e -> {
                 MainWindow.getWindowPane().removeAll();
-                MainWindow.getWindowPane().repaint();
+                ExactCubes.setComponents(MainWindow.getWindowPane());
+                MainWindow.packJFrame();
+                MainWindow.repaintJFrame();
             };
         } /*else if (type == ButtonType.FACTORIAL) {
 
@@ -68,7 +102,50 @@ public abstract class PanelComponents {
 
         } else if (type == ButtonType.TRIPLES) {
 
-        }*/ else if (type == ButtonType.EXIT) {
+        }*/ else if (type == ButtonType.BACK) {
+            return e -> {
+                MainWindow.getWindowPane().removeAll();
+                Selection.setComponents(MainWindow.getWindowPane());
+                MainWindow.packJFrame();
+                MainWindow.repaintJFrame();
+            };
+        } else if (type == ButtonType.EXIT) {
+            return e -> {
+                MainWindow.disposeJFrame();
+                RequestManager.killExecutor();
+            };
+        }
+        // shut up compiler
+        // will never happen
+        return null;
+    }
+
+    private static ActionListener getListener(ButtonType type, JTextArea outputField, JTextField inputField) {
+
+        /*if (type == ButtonType.HELP) {
+
+        } else if (type == ButtonType.CREDITS) {
+
+        } else*/ if (type == ButtonType.EXACTCUBES) {
+            return e -> new ExactCubesRequest(outputField, inputField.getText());
+        } /*else if (type == ButtonType.FACTORIAL) {
+
+        } else if (type == ButtonType.RANDOMINTS) {
+
+        } else if (type == ButtonType.GUESSING) {
+
+        } else if (type == ButtonType.PALINDROME) {
+
+        } else if (type == ButtonType.TRIPLES) {
+
+        }*/ else if (type == ButtonType.BACK) {
+            return e -> {
+                MainWindow.getWindowPane().removeAll();
+                Selection.setComponents(MainWindow.getWindowPane());
+                MainWindow.packJFrame();
+                MainWindow.getWindowPane().repaint();
+            };
+        } else if (type == ButtonType.EXIT) {
             return e -> {
                 MainWindow.disposeJFrame();
                 RequestManager.killExecutor();
