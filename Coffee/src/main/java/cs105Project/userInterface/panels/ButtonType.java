d7fdeaf45c42d2fbbea08ca5de26a60151790ad0
OpenSrcerer <weedpot@fhf.edu.al>
2020-11-14 16:38:41 +0200

    Added a BACK buttontype that returns the user to the main menu.

--- Coffee/src/main/java/cs105Project/userInterface/panels/ButtonType.java
+++ Coffee/src/main/java/cs105Project/userInterface/panels/ButtonType.java
@@ -2,7 +2,7 @@ package cs105Project.userInterface.panels;
 
 public enum ButtonType {
     // UTILITY BUTTONS
-    HELP, CREDITS, EXIT,
+    HELP, CREDITS, EXIT, BACK,
     // ACTION BUTTONS
     EXACTCUBES, FACTORIAL, RANDOMINTS,
     GUESSING, PALINDROME, TRIPLES
