59982355ce713e25cf6e43ba7673b757380cf294
OpenSrcerer <weedpot@fhf.edu.al>
2020-11-15 15:46:01 +0200

    Added higher and lower bounds, progress bar, changed font.

--- Coffee/src/main/java/cs105Project/actions/exactCubes/ExactCubesRequest.java
+++ Coffee/src/main/java/cs105Project/actions/exactCubes/ExactCubesRequest.java
@@ -7,53 +7,65 @@ import javax.swing.*;
 import java.util.ArrayList;
 
 /**
- * Menu option 1: Exact cubes. Calculate and display the list of all integer numbers between 0
- * and 500 which are the exact cube of another integer. Your program should also calculate the sum,
+ * Menu option 1: Exact cubes. Calculate and display the list of all integer numbers between the lowest bound
+ * and the highest bound which are the exact cube of another integer. Your program should also calculate the sum,
  * average, statistical variance and standard deviation of all exact cubes it discovers within the
  * prescribed range. Example: integer 8 is the exact cube of integer 2, 27 is the cube of integer 3, 64
- * is the cube of 4, etc. You may need to look up pseudorandom number generation functions and
- * the relevant mathematical formulae for the statistical metrics (choose the population formula to
- * calculate standard deviation, rather than the one for sample.)
+ * is the cube of 4, etc.
  */
 
 public class ExactCubesRequest implements Request {
     private final CandidateRoot candidate = CandidateRoot.getInstance();
-    private final ArrayList<Integer> exactCubes = new ArrayList<>();
-    private final ArrayList<Integer> correspondingRoots = new ArrayList<>();
-    private final StringBuilder builder = new StringBuilder();
-
-    double sum = 0;
-    double mean;
-    double variance;
-    double stdDev;
 
     // Nullable values
-    int limit;
-    private JTextArea outputField;
+    private int highestBound;
+    private int lowestBound;
 
-    public ExactCubesRequest(JTextArea outputField, String limit) {
+    // Output UI References & Variables
+    private JTextArea outputField;
+    private JProgressBar progressBar;
+
+    public ExactCubesRequest(JTextArea outputField, JProgressBar bar, String lowestBound, String highestBound) {
         try {
-            this.limit = Integer.parseInt(limit);
+            this.lowestBound = Integer.parseInt(lowestBound);
+            this.highestBound = Integer.parseInt(highestBound);
+            if (this.highestBound <= 0 || this.highestBound == Integer.MAX_VALUE || this.lowestBound <= 0 || this.lowestBound >= this.highestBound)
+                throw new NumberFormatException();
         } catch (NumberFormatException ex) {
-            outputField.setText("Invalid input!");
+            outputField.setText("How about you insert something that works?");
             return;
         }
+
         this.outputField = outputField;
+        this.progressBar = bar;
 
         try {
             RequestManager.queueRequest(this);
         } catch (InterruptedException interruptedException) {
-            outputField.setText("Whoops, seems like you broke the program. \nPlease try again.");
+            outputField.setText("Oops, seems like you broke the program. \nPlease try again.");
         }
     }
 
     @Override
     public void run() {
-        for (int numberToTest = 1; numberToTest < limit; ++numberToTest) {
+        final ArrayList<Integer> exactCubes = new ArrayList<>();
+        final ArrayList<Integer> correspondingRoots = new ArrayList<>();
+        final StringBuilder builder = new StringBuilder();
+
+        double sum = 0;
+        double mean;
+        double variance;
+        double stdDev;
+
+        // Estimate for the progress bar
+        double expectedCubes = (int) Math.pow((highestBound - lowestBound), 1/3d);
+
+        for (int numberToTest = lowestBound; numberToTest <= highestBound; ++numberToTest) {
             candidate.setCandidate(numberToTest);
 
             if (candidate.isExactRoot()) {
                 exactCubes.add(numberToTest);
+                updateProgressBar(progressBar, (int) ((exactCubes.size() / expectedCubes) * 100));
                 correspondingRoots.add(candidate.getRoundedRoot());
                 sum = sum + numberToTest;
             }
@@ -71,19 +83,24 @@ public class ExactCubesRequest implements Request {
             stdDev = Math.sqrt(variance);
         }
 
-        int index;
-        for (index = 0; index < exactCubes.size(); ++index) {
-            builder.append(exactCubes.get(index)).append(" is the cube of ").append(correspondingRoots.get(index)).append(".\n");
+        int index = 0;
+        if (sum == 0) {
+            builder.append("----------- No cubes found in this range :( -----------").append("\n");
+        } else {
+            builder.append("----------- Cubes Found -----------").append("\n");
+            for (index = 0; index < exactCubes.size(); ++index) {
+                builder.append(exactCubes.get(index)).append(" is the cube of ").append(correspondingRoots.get(index)).append(".\n");
+            }
+            builder.append("\n----------- Statistics -----------").append("\n");
+            builder.append("Number of cubes found: ").append(exactCubes.size()).append("\n");
+            builder.append("Sum: ").append(sum).append("\n");
+            builder.append("Mean: ").append(mean).append("\n");
+            builder.append("Variance: ").append(variance).append("\n");
+            builder.append("Population Standard Deviation: ").append(stdDev).append("\n");
         }
-        builder.append("Number of cubes found: ").append(exactCubes.size()).append("\n");
-        builder.append("Sum: ").append(sum).append("\n");
-        builder.append("Mean: ").append(mean).append("\n");
-        builder.append("Variance: ").append(variance).append("\n");
-        builder.append("Population Standard Deviation: ").append(stdDev).append("\n");
 
-        // set field rows to fit in result exactly
-        outputField.setRows(index + 5);
-        outputField.setText(builder.toString());
-        outputField.setCaretPosition(outputField.getDocument().getLength() - 1);
+        // Update output field
+        updateOutputArea(outputField, builder.toString(), index + 8);
+        updateProgressBar(progressBar, 0);
     }
 }
--- Coffee/src/main/java/cs105Project/userInterface/panels/ExactCubes.java
+++ Coffee/src/main/java/cs105Project/userInterface/panels/ExactCubes.java
@@ -5,35 +5,62 @@ import cs105Project.userInterface.PanelComponents;
 import javax.swing.*;
 import java.awt.*;
 
-import static cs105Project.userInterface.PanelComponents.discordGray;
-import static cs105Project.userInterface.PanelComponents.setBackgrounds;
+import static cs105Project.userInterface.PanelComponents.*;
 
 public final class ExactCubes {
 
     public static void setComponents(final Container pane) {
         // Init all the JPanels necessary for this program.
-        JPanel inputHolder = new JPanel(); // Using FlowLayout
+        JPanel titleHolder = new JPanel();
+        JPanel barHolder = new JPanel();
+        titleHolder.setLayout(new BoxLayout(titleHolder, BoxLayout.PAGE_AXIS));
+        JPanel inputHolderL = new JPanel();
+        inputHolderL.setLayout(new BoxLayout(inputHolderL, BoxLayout.PAGE_AXIS));
+        JPanel inputHolderR = new JPanel();
+        inputHolderR.setLayout(new BoxLayout(inputHolderR, BoxLayout.PAGE_AXIS));
+        JPanel inputHolderBoth = new JPanel();
         JPanel buttonLayout = new JPanel();
         JPanel buttonHolder = new JPanel(new CardLayout());
+        JPanel buttonAndBarHolder = new JPanel(new BorderLayout());
         JPanel interactionHolder = new JPanel(new BorderLayout());
         JPanel scrollHolder = new JPanel(new GridLayout());
 
-        setBackgrounds(buttonHolder, interactionHolder, scrollHolder, inputHolder, buttonLayout);
+        setBackgrounds(buttonHolder, interactionHolder, scrollHolder, buttonLayout, inputHolderL, inputHolderR, inputHolderBoth, titleHolder, barHolder);
 
         JTextArea outputField = PanelComponents.getJTextArea(15,30);
-        JTextField inputField = PanelComponents.getJTextField("0", 10);
-        JTextField inputField2 = PanelComponents.getJTextField("500", 10);
-        outputField.setPreferredSize(new Dimension(200, 200));
-        outputField.setEditable(false);
+        JProgressBar progressBar = PanelComponents.getProgressBar();
+        JTextField inputFieldL = PanelComponents.getJTextField("1", 10);
+        JTextField inputFieldR = PanelComponents.getJTextField("500", 10);
 
-        // Add components to respective fields
-        inputHolder.add(inputField);
-        inputHolder.add(inputField2);
-        buttonLayout.add(PanelComponents.getButton("Run", ButtonType.EXACTCUBES, outputField, inputField));
+
+        titleHolder.add(getJLabel("Exact Cubes Calculator", titleFont));
+        titleHolder.add(getJLabel("Searches for exact cubes of numbers,", descriptionFont));
+        titleHolder.add(getJLabel("in the range provided below:", descriptionFont));
+        titleHolder.add(Box.createVerticalStrut(5));
+        titleHolder.add(getJSeparator());
+
+        inputHolderL.add(getJLabel("From:", descriptionFont));
+        inputHolderL.add(inputFieldL);
+
+        inputHolderR.add(getJLabel("To:", descriptionFont));
+        inputHolderR.add(inputFieldR);
+
+        inputHolderBoth.add(inputHolderL);
+        inputHolderBoth.add(Box.createHorizontalStrut(50));
+        inputHolderBoth.add(inputHolderR);
+
+        buttonLayout.add(PanelComponents.getButton("Run", ButtonType.EXACTCUBES, progressBar, outputField, inputFieldL, inputFieldR));
         buttonLayout.add(PanelComponents.getButton("Back", ButtonType.BACK));
         buttonHolder.add(buttonLayout);
-        interactionHolder.add(inputHolder, BorderLayout.PAGE_START);
-        interactionHolder.add(buttonHolder, BorderLayout.CENTER);
+
+        barHolder.add(progressBar);
+
+        buttonAndBarHolder.add(buttonHolder, BorderLayout.NORTH);
+        buttonAndBarHolder.add(barHolder, BorderLayout.SOUTH);
+
+        interactionHolder.add(titleHolder, BorderLayout.PAGE_START);
+        interactionHolder.add(inputHolderBoth, BorderLayout.CENTER);
+        interactionHolder.add(buttonAndBarHolder, BorderLayout.SOUTH);
 
         // Create a scroll pane as holder for the output
         JScrollPane scroll = new JScrollPane(
@@ -44,11 +71,15 @@ public final class ExactCubes {
         scroll.setBackground(discordGray);
         scrollHolder.add(scroll);
 
+        JSplitPane splitPane = new JSplitPane(
+                JSplitPane.VERTICAL_SPLIT,
+                interactionHolder, scrollHolder
+        );
+        splitPane.setEnabled(false);
+        splitPane.setDividerSize(1);
+
         pane.add(
-                new JSplitPane(
-                        JSplitPane.VERTICAL_SPLIT,
-                        interactionHolder, scrollHolder
-                ),
+                splitPane,
                 BorderLayout.CENTER
         );
     }
