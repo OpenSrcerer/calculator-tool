65610a28966b28a8ce386b3d3f0c10f8805c021e
OpenSrcerer <weedpot@fhf.edu.al>
2020-11-16 10:55:27 +0200

    Created code for second request.

--- Coffee/src/main/java/cs105Project/actions/factorial/FactorialRequest.java
+++ Coffee/src/main/java/cs105Project/actions/factorial/FactorialRequest.java
@@ -2,9 +2,40 @@ package cs105Project.actions.factorial;
 
 import cs105Project.actions.Request;
 
+import java.util.stream.IntStream;
+
 public class FactorialRequest implements Request {
+
+
     @Override
     public void run() {
+        int[] ints = {3, 4, 5, 6, 7};
+        double average = IntStream.of(ints).sum() / 5d;
+        int closest = getClosestToAverage(ints, average);
+        long factorial = getFactorial(closest);
+    }
 
+    public static int getClosestToAverage(int[] ints, double average) {
+        int closest = 0;
+        double delta = Double.MAX_VALUE;
+
+        for (int i : ints) {
+            if (Math.abs(average - i) < delta) {
+                closest = i;
+                delta = Math.abs(average - i);
+            }
+        }
+
+        return closest;
+    }
+
+    public static long getFactorial(int integer) {
+        long returnValue = integer;
+
+        for (long index = integer - 1; index >= 1; --index) {
+            returnValue = returnValue * index;
+        }
+
+        return returnValue;
     }
 }
