62fbe7181eccdaff7687fb9203e1eb1ea5790e17
OpenSrcerer <weedpot@fhf.edu.al>
2020-11-17 19:03:59 +0200

    Added guessing game program test.

--- Coffee/src/main/java/cs105Project/actions/guessing/GuessingRequest.java
+++ Coffee/src/main/java/cs105Project/actions/guessing/GuessingRequest.java
@@ -1,10 +1,23 @@
 package cs105Project.actions.guessing;
 
 import cs105Project.actions.Request;
+import java.util.Random;
 
 public class GuessingRequest implements Request {
     @Override
     public void run() {
+        int randomValue = new Random().nextInt(66);
+        // get some shit user input
+        int userInput = 0;
 
+        while (true) {
+            if (userInput == randomValue) {
+                System.out.println("Yes");
+            } else if (userInput > randomValue) {
+                System.out.println("Lower");
+            } else {
+                System.out.println("Higher");
+            }
+        }
     }
 }
