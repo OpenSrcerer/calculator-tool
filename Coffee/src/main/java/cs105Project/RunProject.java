3f33b888d71eeb9ff4fb3469580785b934eeabe6
OpenSrcerer <weedpot@fhf.edu.al>
2020-11-18 14:33:09 +0200

    Removed pythagorean triples test code from RunProject and moved it to its own class, TriplesRequest

--- Coffee/src/main/java/cs105Project/RunProject.java
+++ Coffee/src/main/java/cs105Project/RunProject.java
@@ -27,83 +27,13 @@ import java.util.stream.IntStream;
 
 public class RunProject {
 
-    private static final ArrayList<List<Integer>> primitiveRatios = new ArrayList<>();
-    private static final ArrayList<List<Integer>> scaledRatios = new ArrayList<>();
-
     public static void main(String[] args) throws InterruptedException {
-        /*try {
+        try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException | ClassNotFoundException e) {
             e.printStackTrace();
         }
 
-        SwingUtilities.invokeLater(MainWindow::createAndShowGUI);*/
-
-        primitiveRatios.clear();
-        scaledRatios.clear();
-
-        int a, b, c;
-        int m = 2;
-
-        while (m < 100) {
-            int n = 1;
-            while (n < m) {
-
-                a = (m * m) - (n * n);
-                b = 2 * m * n;
-                c = (m * m) + (n * n);
-
-                if (c >= 100)
-                    break;
-
-                if ((a * a) + (b * b) == (c * c)) {
-                    List<Integer> ratio = Arrays.asList(a, b, c);
-                    ratio.sort(Comparator.naturalOrder());
-
-                    if (!isRatioScaled(ratio)) {
-                        primitiveRatios.add(ratio);
-                    }
-                }
-                ++n;
-            }
-            ++m;
-        }
-
-        for (List<Integer> ratio : primitiveRatios) {
-            int index = 2;
-            while (ratio.get(2) * index <= 100) {
-                scaledRatios.add(multiplyListBy(ratio, index));
-                ++index;
-            }
-        }
-
-        System.out.println("------ Primitive Ratios -------");
-        for (List<Integer> ratio : primitiveRatios) {
-            System.out.println(ratio);
-        }
-
-        System.out.println("------ Scaled Ratios -------");
-        for (List<Integer> ratio : scaledRatios) {
-            System.out.println("[" + ratio + "]");
-        }
-    }
-
-    public static boolean isRatioScaled(List<Integer> ratio) {
-        for (List<Integer> primitiveRatio : primitiveRatios) {
-            double div1 = (double) ratio.get(0) / primitiveRatio.get(0);
-            double div2 = (double) ratio.get(1) / primitiveRatio.get(1);
-            double div3 = (double) ratio.get(2) / primitiveRatio.get(2);
-
-            double diva = (div1 + div2 + div3) / 3;
-
-            if (diva == div1) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static List<Integer> multiplyListBy(List<Integer> list, int multiplier) {
-        return list.stream().map(e -> e * multiplier).collect(Collectors.toList());
+        SwingUtilities.invokeLater(MainWindow::createAndShowGUI);
     }
 }
--- Coffee/src/main/java/cs105Project/actions/triples/TriplesRequest.java
+++ Coffee/src/main/java/cs105Project/actions/triples/TriplesRequest.java
@@ -2,9 +2,81 @@ package cs105Project.actions.triples;
 
 import cs105Project.actions.Request;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static cs105Project.actions.Functions.multiplyListBy;
+
 public class TriplesRequest implements Request {
+
+    private static final ArrayList<List<Integer>> primitiveRatios = new ArrayList<>();
+    private static final ArrayList<List<Integer>> scaledRatios = new ArrayList<>();
+
     @Override
     public void run() {
+        primitiveRatios.clear();
+        scaledRatios.clear();
 
+        int a, b, c;
+        int m = 2;
+
+        while (m < 100) {
+            int n = 1;
+            while (n < m) {
+
+                a = (m * m) - (n * n);
+                b = 2 * m * n;
+                c = (m * m) + (n * n);
+
+                if (c >= 100)
+                    break;
+
+                if ((a * a) + (b * b) == (c * c)) {
+                    List<Integer> ratio = Arrays.asList(a, b, c);
+                    ratio.sort(Comparator.naturalOrder());
+
+                    if (!isRatioScaled(ratio)) {
+                        primitiveRatios.add(ratio);
+                    }
+                }
+                ++n;
+            }
+            ++m;
+        }
+
+        for (List<Integer> ratio : primitiveRatios) {
+            int index = 2;
+            while (ratio.get(2) * index <= 100) {
+                scaledRatios.add(multiplyListBy(ratio, index));
+                ++index;
+            }
+        }
+
+        System.out.println("------ Primitive Ratios -------");
+        for (List<Integer> ratio : primitiveRatios) {
+            System.out.println(ratio);
+        }
+
+        System.out.println("------ Scaled Ratios -------");
+        for (List<Integer> ratio : scaledRatios) {
+            System.out.println("[" + ratio + "]");
+        }
+    }
+
+    private static boolean isRatioScaled(List<Integer> ratio) {
+        for (List<Integer> primitiveRatio : primitiveRatios) {
+            double div1 = (double) ratio.get(0) / primitiveRatio.get(0);
+            double div2 = (double) ratio.get(1) / primitiveRatio.get(1);
+            double div3 = (double) ratio.get(2) / primitiveRatio.get(2);
+
+            double diva = (div1 + div2 + div3) / 3;
+
+            if (diva == div1) {
+                return true;
+            }
+        }
+        return false;
     }
 }
