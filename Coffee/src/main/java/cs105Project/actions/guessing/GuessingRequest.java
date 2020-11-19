aaa0f62ebb78c4d52b7c5a67d08617aa0cf6752b
OpenSrcerer <weedpot@fhf.edu.al>
2020-11-20 00:55:46 +0200

    Added first version of working Guessing and GuessingRequest GUI.

--- Coffee/src/main/java/cs105Project/actions/guessing/GuessingRequest.java
+++ Coffee/src/main/java/cs105Project/actions/guessing/GuessingRequest.java
@@ -1,23 +1,69 @@
 package cs105Project.actions.guessing;
 
 import cs105Project.actions.Request;
+import cs105Project.managers.RequestManager;
+
+import javax.swing.*;
 import java.util.Random;
 
 public class GuessingRequest implements Request {
+
+    private static int numberToGuess = new Random().nextInt(66);
+    private static int guesses = 0;
+
+    private int userInput;
+
+    private final JTextArea outputArea;
+    private final JButton button;
+
+    public GuessingRequest(JTextArea outputArea, JButton button, String userInput) {
+        this.outputArea = outputArea;
+        this.button = button;
+
+        try {
+            this.userInput = Integer.parseInt(userInput);
+            if (this.userInput <= 0 || this.userInput >= 66)
+                throw new NumberFormatException();
+        } catch (NumberFormatException ex) {
+            updateOutputArea(outputArea, outputArea.getText() + "How about you insert something that works?\n", outputArea.getRows() + 1);
+            return;
+        }
+
+        RequestManager.queueRequest(this);
+    }
+
     @Override
     public void run() {
-        int randomValue = new Random().nextInt(66);
-        // get some shit user input
-        int userInput = 0;
+        toggleRunButton(button);
+        ++guesses;
 
-        while (true) {
-            if (userInput == randomValue) {
-                System.out.println("Yes");
-            } else if (userInput > randomValue) {
-                System.out.println("Lower");
-            } else {
-                System.out.println("Higher");
-            }
+        if (userInput == numberToGuess) {
+            updateOutputArea(outputArea,
+                    outputArea.getText() + "----------------------------------------------------------------\n"
+                            + "Congratulations, you have guessed correctly! The number was "
+                            + numberToGuess +".\n"
+                            + "Total Guesses: " + guesses +".\n"
+                            + "A new number has been picked, you may start guessing again.\n",
+                    outputArea.getRows() + 4
+            );
+            numberToGuess = new Random().nextInt(66);
+            guesses = 0;
+        } else if (userInput > numberToGuess) {
+            updateOutputArea(outputArea,
+                    outputArea.getText() + "----------------------------------------------------------------\n"
+                    + "You need to guess lower!\n"
+                    + "Current guesses: " + guesses + ".\n",
+                    outputArea.getRows() + 3
+            );
+        } else {
+            updateOutputArea(outputArea,
+                    outputArea.getText() + "----------------------------------------------------------------\n"
+                    + "You need to guess higher!\n"
+                    + "Current guesses: " + guesses + ".\n",
+                    outputArea.getRows() + 3
+            );
         }
+
+        toggleRunButton(button);
     }
 }
--- Coffee/src/main/java/cs105Project/userInterface/panels/Guessing.java
+++ Coffee/src/main/java/cs105Project/userInterface/panels/Guessing.java
@@ -11,54 +11,38 @@ public final class Guessing {
     public static void setComponents(final Container pane) {
         // Init all the JPanels necessary for this program.
         final JPanel titleHolder = new JPanel();
-        final JPanel barHolder = new JPanel();
         titleHolder.setLayout(new BoxLayout(titleHolder, BoxLayout.PAGE_AXIS));
-        final JPanel inputHolderL = new JPanel();
-        inputHolderL.setLayout(new BoxLayout(inputHolderL, BoxLayout.PAGE_AXIS));
-        final JPanel inputHolderR = new JPanel();
-        inputHolderR.setLayout(new BoxLayout(inputHolderR, BoxLayout.PAGE_AXIS));
-        final JPanel inputHolderBoth = new JPanel();
+        final JPanel inputHolder = new JPanel();
+        inputHolder.setLayout(new BoxLayout(inputHolder, BoxLayout.PAGE_AXIS));
+        final JPanel inputPanel = new JPanel();
         final JPanel buttonLayout = new JPanel();
         final JPanel buttonHolder = new JPanel(new CardLayout());
-        final JPanel buttonAndBarHolder = new JPanel(new BorderLayout());
         final JPanel interactionHolder = new JPanel(new BorderLayout());
         final JPanel scrollHolder = new JPanel(new GridLayout());
 
-        setBackgrounds(buttonHolder, interactionHolder, scrollHolder, buttonLayout, inputHolderL, inputHolderR, inputHolderBoth, titleHolder, barHolder);
+        setBackgrounds(buttonHolder, interactionHolder, scrollHolder, buttonLayout, inputHolder, titleHolder, inputPanel);
 
         JTextArea outputField = PanelComponents.getTextArea(15,30);
-        JProgressBar progressBar = PanelComponents.getProgressBar();
-        JTextField inputFieldL = PanelComponents.getTextField("1", 10);
-        JTextField inputFieldR = PanelComponents.getTextField("500", 10);
+        JTextField inputField = PanelComponents.getTextField("1", 4);
 
-        titleHolder.add(getLabel("Exact Cubes Calculator", titleFont));
-        titleHolder.add(getLabel("Searches for exact cubes of numbers,", descriptionFont));
-        titleHolder.add(getLabel("in the range provided below:", descriptionFont));
+        titleHolder.add(getLabel("Number Guessing Game", titleFont));
+        titleHolder.add(getLabel("Guess a number, 1-64.", descriptionFont));
+        titleHolder.add(getLabel("Put your guess in the box below.", descriptionFont));
         titleHolder.add(Box.createVerticalStrut(5));
         titleHolder.add(getSeparator());
 
-        inputHolderL.add(getLabel("From:", descriptionFont));
-        inputHolderL.add(inputFieldL);
+        inputHolder.add(getLabel("Your Guess:", descriptionFont));
+        inputHolder.add(inputField);
 
-        inputHolderR.add(getLabel("To:", descriptionFont));
-        inputHolderR.add(inputFieldR);
+        inputPanel.add(inputHolder);
 
-        inputHolderBoth.add(inputHolderL);
-        inputHolderBoth.add(Box.createHorizontalStrut(50));
-        inputHolderBoth.add(inputHolderR);
-
-        buttonLayout.add(PanelComponents.getButton("Run", ButtonType.EXACTCUBES, outputField, progressBar, inputFieldL, inputFieldR));
+        buttonLayout.add(PanelComponents.getButton("Guess", ButtonType.GUESSING, outputField, inputField));
         buttonLayout.add(PanelComponents.getButton("Back", ButtonType.BACK));
         buttonHolder.add(buttonLayout);
 
-        barHolder.add(progressBar);
-
-        buttonAndBarHolder.add(buttonHolder, BorderLayout.NORTH);
-        buttonAndBarHolder.add(barHolder, BorderLayout.SOUTH);
-
         interactionHolder.add(titleHolder, BorderLayout.PAGE_START);
-        interactionHolder.add(inputHolderBoth, BorderLayout.CENTER);
-        interactionHolder.add(buttonAndBarHolder, BorderLayout.SOUTH);
+        interactionHolder.add(inputPanel, BorderLayout.CENTER);
+        interactionHolder.add(buttonHolder, BorderLayout.SOUTH);
 
         // Create a scroll pane as holder for the output
         JScrollPane scroll = new JScrollPane(
