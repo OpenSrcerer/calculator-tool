f7852d56016ce85bde92ea5cf811f3463e68cc8f
OpenSrcerer <weedpot@fhf.edu.al>
2020-11-19 18:25:22 +0200

    Added Factorial GUI code and made FactorialRequest code to work with the GUI

--- Coffee/src/main/java/cs105Project/actions/factorial/FactorialRequest.java
+++ Coffee/src/main/java/cs105Project/actions/factorial/FactorialRequest.java
@@ -1,21 +1,55 @@
 package cs105Project.actions.factorial;
 
 import cs105Project.actions.Request;
+import cs105Project.managers.RequestManager;
 
+import javax.swing.*;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class FactorialRequest implements Request {
 
+    private final int[] numbers = new int[5];
+
+    private final JTextArea outputArea;
+    private final JButton button;
+
+    public FactorialRequest(JTextArea outputArea, JButton button, JTextField[] fields) {
+        this.outputArea = outputArea;
+        this.button = button;
+
+        for (int index = 0; index < 5; ++index) {
+            try {
+                numbers[index] = Integer.parseInt(fields[index].getText());
+            } catch (NumberFormatException ex) {
+                updateOutputArea(outputArea, "How about you insert something that works?", 1);
+                return;
+            }
+        }
+
+        RequestManager.queueRequest(this);
+    }
 
     @Override
     public void run() {
-        int[] ints = {3, 4, 5, 6, 7};
-        double average = IntStream.of(ints).sum() / 5d;
-        int closest = getClosestToAverage(ints, average);
+        toggleRunButton(button);
+
+        double average = IntStream.of(numbers).sum() / 5d;
+        int closest = getClosestToAverage(numbers, average);
         long factorial = getFactorial(closest);
+
+        updateOutputArea(outputArea,
+                outputArea.getText() + "----------------------------------------------------------------\n"
+                        + "For the given numbers: "
+                        + Arrays.toString(numbers) + ".\n"
+                        + "Average: " + average + "\n"
+                        + "Closest integer: " + closest + "\n"
+                        + "Factorial of closest integer: " + factorial + "\n",
+                outputArea.getRows() + 5);
+        toggleRunButton(button);
     }
 
-    public static int getClosestToAverage(int[] ints, double average) {
+    private static int getClosestToAverage(int[] ints, double average) {
         int closest = 0;
         double delta = Double.MAX_VALUE;
 
--- Coffee/src/main/java/cs105Project/userInterface/panels/Factorial.java
+++ Coffee/src/main/java/cs105Project/userInterface/panels/Factorial.java
@@ -12,58 +12,73 @@ public final class Factorial {
     public static void setComponents(final Container pane) {
         // Init all the JPanels necessary for this program.
         final JPanel titleHolder = new JPanel();
-        final JPanel barHolder = new JPanel();
         titleHolder.setLayout(new BoxLayout(titleHolder, BoxLayout.PAGE_AXIS));
-        final JPanel inputHolderL = new JPanel();
-        inputHolderL.setLayout(new BoxLayout(inputHolderL, BoxLayout.PAGE_AXIS));
-        final JPanel inputHolderR = new JPanel();
-        inputHolderR.setLayout(new BoxLayout(inputHolderR, BoxLayout.PAGE_AXIS));
-        final JPanel inputHolderBoth = new JPanel();
+
+        // Input Holders
+        final JPanel inputHolder1 = new JPanel();
+        inputHolder1.setLayout(new BoxLayout(inputHolder1, BoxLayout.PAGE_AXIS));
+        final JPanel inputHolder2 = new JPanel();
+        inputHolder2.setLayout(new BoxLayout(inputHolder2, BoxLayout.PAGE_AXIS));
+        final JPanel inputHolder3 = new JPanel();
+        inputHolder3.setLayout(new BoxLayout(inputHolder3, BoxLayout.PAGE_AXIS));
+        final JPanel inputHolder4 = new JPanel();
+        inputHolder4.setLayout(new BoxLayout(inputHolder4, BoxLayout.PAGE_AXIS));
+        final JPanel inputHolder5 = new JPanel();
+        inputHolder5.setLayout(new BoxLayout(inputHolder5, BoxLayout.PAGE_AXIS));
+        final JPanel inputHolderAll = new JPanel();
+
         final JPanel buttonLayout = new JPanel();
         final JPanel buttonHolder = new JPanel(new CardLayout());
-        final JPanel buttonAndBarHolder = new JPanel(new BorderLayout());
         final JPanel interactionHolder = new JPanel(new BorderLayout());
         final JPanel scrollHolder = new JPanel(new GridLayout());
 
-        setBackgrounds(buttonHolder, interactionHolder, scrollHolder, buttonLayout, inputHolderL, inputHolderR, inputHolderBoth, titleHolder, barHolder);
+        setBackgrounds(buttonHolder, interactionHolder, scrollHolder, buttonLayout, inputHolder1,
+                inputHolder2, inputHolder3, inputHolder4, inputHolder5, inputHolderAll, titleHolder);
 
-        JTextArea outputField = PanelComponents.getTextArea(15,30);
-        JProgressBar progressBar = PanelComponents.getProgressBar();
-        JTextField inputFieldL = PanelComponents.getTextField("1", 10);
-        JTextField inputFieldR = PanelComponents.getTextField("500", 10);
+        JTextArea outputArea = PanelComponents.getTextArea(15,30);
+        JTextField[] inputFields = {
+                PanelComponents.getTextField("1", 4),
+                PanelComponents.getTextField("1", 4),
+                PanelComponents.getTextField("1", 4),
+                PanelComponents.getTextField("1", 4),
+                PanelComponents.getTextField("1", 4)
+        };
 
-        titleHolder.add(getLabel("Exact Cubes Calculator", titleFont));
-        titleHolder.add(getLabel("Searches for exact cubes of numbers,", descriptionFont));
-        titleHolder.add(getLabel("in the range provided below:", descriptionFont));
+        titleHolder.add(getLabel("Factorial of the Average", titleFont));
+        titleHolder.add(getLabel("Given the five numbers below, this program", descriptionFont));
+        titleHolder.add(getLabel("calculates their average and the factorial", descriptionFont));
+        titleHolder.add(getLabel("of the number closest to the group's average.", descriptionFont));
         titleHolder.add(Box.createVerticalStrut(5));
         titleHolder.add(getSeparator());
 
-        inputHolderL.add(getLabel("From:", descriptionFont));
-        inputHolderL.add(inputFieldL);
+        inputHolder1.add(getLabel("N1:", descriptionFont));
+        inputHolder1.add(inputFields[0]);
+        inputHolder2.add(getLabel("N2:", descriptionFont));
+        inputHolder2.add(inputFields[1]);
+        inputHolder3.add(getLabel("N3:", descriptionFont));
+        inputHolder3.add(inputFields[2]);
+        inputHolder4.add(getLabel("N4:", descriptionFont));
+        inputHolder4.add(inputFields[3]);
+        inputHolder5.add(getLabel("N5:", descriptionFont));
+        inputHolder5.add(inputFields[4]);
 
-        inputHolderR.add(getLabel("To:", descriptionFont));
-        inputHolderR.add(inputFieldR);
+        inputHolderAll.add(inputHolder1);
+        inputHolderAll.add(inputHolder2);
+        inputHolderAll.add(inputHolder3);
+        inputHolderAll.add(inputHolder4);
+        inputHolderAll.add(inputHolder5);
 
-        inputHolderBoth.add(inputHolderL);
-        inputHolderBoth.add(Box.createHorizontalStrut(50));
-        inputHolderBoth.add(inputHolderR);
-
-        buttonLayout.add(PanelComponents.getButton("Run", ButtonType.EXACTCUBES, outputField, progressBar, inputFieldL, inputFieldR));
+        buttonLayout.add(PanelComponents.getButton("Run", ButtonType.FACTORIAL, outputArea, inputFields));
         buttonLayout.add(PanelComponents.getButton("Back", ButtonType.BACK));
         buttonHolder.add(buttonLayout);
 
-        barHolder.add(progressBar);
-
-        buttonAndBarHolder.add(buttonHolder, BorderLayout.NORTH);
-        buttonAndBarHolder.add(barHolder, BorderLayout.SOUTH);
-
         interactionHolder.add(titleHolder, BorderLayout.PAGE_START);
-        interactionHolder.add(inputHolderBoth, BorderLayout.CENTER);
-        interactionHolder.add(buttonAndBarHolder, BorderLayout.SOUTH);
+        interactionHolder.add(inputHolderAll, BorderLayout.CENTER);
+        interactionHolder.add(buttonHolder, BorderLayout.SOUTH);
 
         // Create a scroll pane as holder for the output
         JScrollPane scroll = new JScrollPane(
-                outputField,
+                outputArea,
                 ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
                 ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
         );
