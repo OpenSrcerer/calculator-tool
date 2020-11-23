62f511c5dd63d50062ce50f5c7db63f48fba4911
OpenSrcerer <weedpot@fhf.edu.al>
2020-11-23 17:34:37 +0200

    Added a description for the ButtonType enum.

--- Coffee/src/main/java/cs105Project/userInterface/panels/ButtonType.java
+++ Coffee/src/main/java/cs105Project/userInterface/panels/ButtonType.java
@@ -1,5 +1,14 @@
 package cs105Project.userInterface.panels;
 
+/**
+ * This enum is used to distinguish button types,
+ * when creating different button action listeners.
+ * For example, a Help type button will show a help GUI, but
+ * a Factorial button will initialize the factorial calculation GUI.
+ *
+ * Using an enum makes it easy to distinguish these buttons by assigning
+ * a ButtonType value to them.
+ */
 public enum ButtonType {
     // UTILITY BUTTONS
     HELP, CREDITS, EXIT, BACK,
