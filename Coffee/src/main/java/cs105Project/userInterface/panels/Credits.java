d65c589d81697a87d3da2bbe8ca57556db864270
OpenSrcerer <weedpot@fhf.edu.al>
2020-11-23 17:34:53 +0200

    Added Credits and Help GUI panels.

---
+++ Coffee/src/main/java/cs105Project/userInterface/panels/Credits.java
@@ -0,0 +1,31 @@
+package cs105Project.userInterface.panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static cs105Project.userInterface.PanelComponents.*;
+
+public class Credits {
+
+    public static void setComponents(final Container pane) {
+        final JPanel infoPanel = getJPanel(BoxLayout.PAGE_AXIS);
+        final JPanel imagePanel = getJPanel();
+        final JPanel buttonPanel = getJPanel();
+        final JPanel totalPanel = getJPanel();
+
+        imagePanel.add(getImageLabel("bonk.png"));
+        buttonPanel.add(getButton("Back", ButtonType.BACK));
+        infoPanel.add(Box.createVerticalStrut(10));
+        infoPanel.add(getLabel("Multitool v0.0.1", titleFont));
+        infoPanel.add(Box.createVerticalStrut(10));
+        infoPanel.add(getLabel("GNU © 2020 Daniel Stefani / OpenSrcerer", descriptionFont));
+        infoPanel.add(getLabel("Made for my final CS105 Project", descriptionFont));
+        infoPanel.add(getLabel("Thank you Bonkers & Marce", descriptionFont));
+        infoPanel.add(buttonPanel);
+
+        totalPanel.add(imagePanel);
+        totalPanel.add(infoPanel);
+
+        pane.add(totalPanel);
+    }
+}
---
+++ Coffee/src/main/java/cs105Project/userInterface/panels/Help.java
@@ -0,0 +1,33 @@
+package cs105Project.userInterface.panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static cs105Project.userInterface.PanelComponents.*;
+
+public final class Help {
+
+    public static void setComponents(final Container pane) {
+        final JPanel infoPanel = getJPanel(BoxLayout.PAGE_AXIS);
+        final JPanel buttonPanel = getJPanel();
+        final JPanel totalPanel = getJPanel();
+
+        buttonPanel.add(getButton("Back", ButtonType.BACK));
+
+        infoPanel.add(getLabel("How to use MultiTool", titleFont));
+        infoPanel.add(Box.createVerticalStrut(10));
+        infoPanel.add(getLabel("This is a very simple GUI program. In the main", descriptionFont));
+        infoPanel.add(getLabel("menu, you can click any of the buttons to", descriptionFont));
+        infoPanel.add(getLabel("access the tool that you need.", descriptionFont));
+        infoPanel.add(Box.createVerticalStrut(10));
+        infoPanel.add(getLabel("Each tool has a short description and only a few", descriptionFont));
+        infoPanel.add(getLabel("inputs, so reading them is all you need to get set up.", descriptionFont));
+        infoPanel.add(Box.createVerticalStrut(10));
+        infoPanel.add(getLabel("Click the button below to go back to the menu.", descriptionFont));
+        infoPanel.add(buttonPanel);
+
+        totalPanel.add(infoPanel);
+
+        pane.add(totalPanel);
+    }
+}
