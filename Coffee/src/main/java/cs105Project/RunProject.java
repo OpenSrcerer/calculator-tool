86bafd08a291bf82ccc03b6b5b229d2468231978
OpenSrcerer <weedpot@fhf.edu.al>
2020-11-23 17:38:08 +0200

    Added project description to RunProject.

--- Coffee/src/main/java/cs105Project/RunProject.java
+++ Coffee/src/main/java/cs105Project/RunProject.java
@@ -1,21 +1,39 @@
+/*
+ * <h3>Made for the Final Project in CS105, due December 4th 2020. <br>
+ * This work is licensed under the GNU General Public License v3.0 <br>
+ * GNU © 2020 OpenSrcerer</h3>
+ */
+
 package cs105Project;
 
 import cs105Project.userInterface.MainWindow;
 
 import javax.swing.*;
-import java.io.IOException;
 
 /**
- * <h1>Java Program</h1>
+ * <h1>A Calculation Multitool</h1>
  *
- * <p>A Java program demonstrate crap</p>
+ * <p>A Java program that fulfills specific requests as required in the Final Course Project Description PDF.<br></p>
+ *
+ * <h2>Available Calculations:</h2>
+ * <ul>
+ *     <li>Exact cubes</li>
+ *     <li>Factorial of the average</li>
+ *     <li>Stats of random integers</li>
+ *     <li>Number guessing game</li>
+ *     <li>Palindrome detection</li>
+ *     <li>Pythagorean triples</li>
+ * </ul>
  *
  * <br>
- * <h3>Made for the Midterm Examination in CS105, November 9th 2020. <br> This work is licensed under the GNU General Public License v3.0</h3>
+ * <h3>Made for the Final Project in CS105, due December 4th 2020. <br> This work is licensed under the GNU General Public License v3.0</h3>
+ * <h5>Note: Special permission has been granted to me by
+ * Dr. Alexander Astaras to bypass the project's requirements. <br>
+ * Instead of using a text-based menu, I have programmed a GUI.</h5>
  *
  * @author Daniel Stefani (OpenSrcerer)
  * @version 0.0.1
- * @since 2020-10-12
+ * @since 2020-11-12
  */
 
 public class RunProject {
